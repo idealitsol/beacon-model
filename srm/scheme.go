31d70a3155a82afc758fdb3be999a271c22406f5
idealitsol <idealitsol@gmail.com>
2019-05-27 17:35:41 +0100

    fixed fields of scheme and course models

--- srm/scheme.go
+++ srm/scheme.go
@@ -2,7 +2,7 @@ package srm
 
 // Scheme model
 type Scheme struct {
-	ID     string `json:"id" gorm:"type:UUID;primary_key;default:gen_random_uuid();size:36"`
+	Id     string `json:"id" gorm:"type:UUID;primary_key;default:gen_random_uuid();size:36"`
 	Scheme string `json:"scheme" gorm:"type:varchar(255);not null;unique_index"`
 	Status bool   `json:"status" gorm:"type:bool;default:false"`
 	// Course *Course `gorm:"ForeignKey:SchemeID;AssociationForeignKey:ID"`
