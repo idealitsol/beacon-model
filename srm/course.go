beac0e6359ed05aefe95b0413f82342a67a92f67
idealitsol <idealitsol@gmail.com>
2019-06-08 16:11:54 +0100

    conformed to golint

--- srm/course.go
+++ srm/course.go
@@ -6,17 +6,19 @@ import (
 
 // Course model
 type Course struct {
-	Id          string  `json:"id" gorm:"type:UUID;primary_key;default:gen_random_uuid();size:36"`
+	ID          string  `json:"id" gorm:"type:UUID;primary_key;default:gen_random_uuid();size:36"`
 	Code        string  `json:"code" gorm:"type:varchar(10);not null;unique_index"`
 	Title       string  `json:"title" gorm:"type:varchar(555); not null;index"`
 	Description string  `json:"description" gorm:"type:varchar(255)"`
 	Credits     float32 `json:"credits" gorm:"type:decimal; default:0.00"`
 	Owner       int32   `json:"owner" gorm:"size:3;not null"`                                             // this is deptID or unitId in legacy system
 	Type        string  `json:"type" gorm:"type:varchar(15);default:'Regular'"`                           // Regular or General Course
-	SchemeId    string  `json:"schemeId" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"` // Default Grading Scheme for the Course
+	SchemeID    string  `json:"schemeId" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"` // Default Grading Scheme for the Course
 	PreCourse   string  `json:"preCourse" gorm:"type:varchar(10)"`                                        // If a course is a prerequisite then we set it here (This is pcrel_preq)
 	Status      bool    `json:"status" gorm:"type:bool;default:false"`
-	Scheme      *Scheme `json:"scheme,omitempty" gorm:"ForeignKey:ID;AssociationForeignKey:SchemeID"`
+	Institution string  `json:"-" gorm:"type:UUID"`
+
+	Scheme *Scheme `json:"scheme,omitempty" gorm:"ForeignKey:ID;AssociationForeignKey:SchemeID"`
 }
 
 // Courses array
@@ -34,11 +36,12 @@ func CourseP2STransformer(data *pbx.Course) Course {
 		SchemeId:    data.GetSchemeId(),
 		PreCourse:   data.GetPreCourse(),
 		Status:      data.GetStatus(),
+		Institution: data.GetInstitution(),
 	}
 
 	// If GetId has no value then it's a POST request (Create)
 	if len(data.GetId()) != 0 {
-		course.Id = data.GetId()
+		course.ID = data.GetId()
 	}
 
 	// includes scheme
@@ -56,7 +59,7 @@ func CourseP2STransformer(data *pbx.Course) Course {
 // CourseS2PTransformer transforms course Struct to Protobuf
 func CourseS2PTransformer(data Course) *pbx.Course {
 	course := &pbx.Course{
-		Id:          data.Id,
+		Id:          data.ID,
 		Code:        data.Code,
 		Title:       data.Title,
 		Description: data.Description,
@@ -71,7 +74,7 @@ func CourseS2PTransformer(data Course) *pbx.Course {
 	// includes scheme
 	if data.Scheme != nil {
 		course.Scheme = &pbx.Scheme{
-			Id:     data.Scheme.Id,
+			Id:     data.Scheme.ID,
 			Scheme: data.Scheme.Scheme,
 			Status: data.Scheme.Status,
 		}
