6645f2fa53004ed4175d379e67b7e603b2ba3674
Emmanuel Kofi Bessah <ekbessah@gmail.com>
2020-01-12 22:33:30 +0000

    add new models

---
+++ oma/configuration.go
@@ -0,0 +1,46 @@
+package oma
+
+import "github.com/idealitsol/beacon-proto/pbx"
+
+// Configuration database model
+type Configuration struct {
+	Key   string `json:"key" gorm:"not null"`
+	Value string `json:"value" gorm:""`
+
+	BXXUpdatedFields []string `json:"-" gorm:"-"`
+}
+
+// Configurations is an array of Configuration objects
+type Configurations []Configuration
+
+// ConfigurationP2STransformer transforms Configuration Protobuf to Struct
+func ConfigurationP2STransformer(data *pbx.Configuration) Configuration {
+	model := Configuration{
+		Value: data.GetValue(),
+
+		BXXUpdatedFields: data.GetBXX_UpdatedFields(),
+	}
+
+	// If GetKey has no value then it's a POST request (Create)
+	if len(data.GetKey()) != 0 {
+		model.Key = data.GetKey()
+	}
+
+	// Handle pointers after this
+
+	return model
+}
+
+// ConfigurationS2PTransformer transforms Configuration Struct to Protobuf
+func ConfigurationS2PTransformer(data Configuration) *pbx.Configuration {
+	model := &pbx.Configuration{
+		Key:   data.Key,
+		Value: data.Value,
+
+		BXX_UpdatedFields: data.BXXUpdatedFields,
+	}
+
+	// Handle pointers after this
+
+	return model
+}
---
+++ oma/session.go
@@ -0,0 +1,63 @@
+package oma
+
+import (
+	"time"
+
+	"github.com/idealitsol/beacon-proto/pbx"
+	util "github.com/idealitsol/beacon-util"
+)
+
+// Session database model
+type Session struct {
+	ID           string     `json:"id" gorm:"type:UUID;primary_key;default:gen_random_uuid();size:36"`
+	Name         string     `json:"name" gorm:"type:varchar(30);not null"`
+	AcademicYear string     `json:"academicYear" gorm:"type:UUID;"`
+	StartDate    *time.Time `json:"startDate"`
+	EndDate      *time.Time `json:"endDate"`
+	PayType      string     `json:"payType" gorm:"type:varchar(15);not null;default:'free'"`
+
+	BXXUpdatedFields []string `json:"-" gorm:"-"`
+}
+
+// Sessions is an array of Session objects
+type Sessions []Session
+
+// SessionP2STransformer transforms Session Protobuf to Struct
+func SessionP2STransformer(data *pbx.Session) Session {
+	model := Session{
+		Name:         data.GetName(),
+		AcademicYear: data.GetAcademicYear(),
+		StartDate:    util.GrpcTimeToGoTime(data.GetStartDate()),
+		EndDate:      util.GrpcTimeToGoTime(data.GetEndDate()),
+		PayType:      data.GetPayType(),
+
+		BXXUpdatedFields: data.GetBXX_UpdatedFields(),
+	}
+
+	// If GetID has no value then it's a POST request (Create)
+	if len(data.GetId()) != 0 {
+		model.ID = data.GetId()
+	}
+
+	// Handle pointers after this
+
+	return model
+}
+
+// SessionS2PTransformer transforms Session Struct to Protobuf
+func SessionS2PTransformer(data Session) *pbx.Session {
+	model := &pbx.Session{
+		Id:           data.ID,
+		Name:         data.Name,
+		AcademicYear: data.AcademicYear,
+		StartDate:    util.GoTimeToGrpcTime(data.StartDate),
+		EndDate:      util.GoTimeToGrpcTime(data.EndDate),
+		PayType:      data.PayType,
+
+		BXX_UpdatedFields: data.BXXUpdatedFields,
+	}
+
+	// Handle pointers after this
+
+	return model
+}
