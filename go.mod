94179308224dcc4ebe5c1835e1e28f1e78cf71b1
Emmanuel Kofi Bessah <ekbessah@gmail.com>
2020-02-09 17:36:49 +0000

    rafactor model

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/idealitsol/beacon-model
 go 1.12
 
 require (
-	github.com/idealitsol/beacon-proto v0.0.67
+	github.com/idealitsol/beacon-proto v0.0.68
 	github.com/idealitsol/beacon-util v0.0.22
 	github.com/jinzhu/gorm v1.9.11
 )
--- oma/appl_aca.go
+++ oma/appl_aca.go
@@ -18,19 +18,12 @@ type ApplAca struct {
 	SchEndDate     *time.Time     `json:"schEndDate"`
 	StudyCampus    string         `json:"studyCampus" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"`
 	StudyCenter    string         `json:"studyCenter" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"`
-	ProgChoice1    string         `json:"progChoice1" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"`
-	ProgChoice2    string         `json:"progChoice2" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"`
-	ProgChoice3    string         `json:"progChoice3" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"`
-	ProgChoice4    string         `json:"progChoice4" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"`
-	ProgChoice5    string         `json:"progChoice5" gorm:"type:UUID;default:'00000000-0000-0000-0000-000000000000'"`
+	ProgChoice     postgres.Jsonb `json:"progChoice" gorm:"type:jsonb;not null;default:'[]'"`
 	FeePaying      bool           `json:"feePaying" gorm:"default:false"`
 	GradResTopic   string         `json:"gradResTopic" gorm:"type:varchar(1000)"`
-	Referee1       postgres.Jsonb `json:"referee1" gorm:"type:jsonb;not null;default:'{}'"`
-	Referee2       postgres.Jsonb `json:"referee2" gorm:"type:jsonb;not null;default:'{}'"`
-	Referee3       postgres.Jsonb `json:"referee3" gorm:"type:jsonb;not null;default:'{}'"`
-	Referee4       postgres.Jsonb `json:"referee4" gorm:"type:jsonb;not null;default:'{}'"`
+	Referee        postgres.Jsonb `json:"referee" gorm:"type:jsonb;not null;default:'[]'"`
 	IsComplete     bool           `json:"isComplete" gorm:"default:false"`
-	InstitutionID  string         `json:"-" gorm:"type:UUID;"`
+	InstitutionID  string         `json:"institutionId" gorm:"type:UUID;"`
 
 	BXXUpdatedFields []string `json:"-" gorm:"-"`
 }
@@ -47,17 +40,10 @@ func ApplAcaP2STransformer(data *pbx.ApplAca) ApplAca {
 		SchEndDate:     util.GrpcTimeToGoTime(data.GetSchEndDate()),
 		StudyCampus:    data.GetStudyCampus(),
 		StudyCenter:    data.GetStudyCenter(),
-		ProgChoice1:    data.GetProgChoice_1(),
-		ProgChoice2:    data.GetProgChoice_2(),
-		ProgChoice3:    data.GetProgChoice_3(),
-		ProgChoice4:    data.GetProgChoice_4(),
-		ProgChoice5:    data.GetProgChoice_5(),
+		ProgChoice:     postgres.Jsonb{json.RawMessage(data.GetProgChoice())},
 		FeePaying:      data.GetFeePaying(),
 		GradResTopic:   data.GetGradResTopic(),
-		Referee1:       postgres.Jsonb{json.RawMessage(data.GetReferee_1())},
-		Referee2:       postgres.Jsonb{json.RawMessage(data.GetReferee_2())},
-		Referee3:       postgres.Jsonb{json.RawMessage(data.GetReferee_3())},
-		Referee4:       postgres.Jsonb{json.RawMessage(data.GetReferee_4())},
+		Referee:        postgres.Jsonb{json.RawMessage(data.GetReferee())},
 		IsComplete:     data.GetIsComplete(),
 		InstitutionID:  data.GetInstitutionId(),
 
@@ -84,17 +70,10 @@ func ApplAcaS2PTransformer(data ApplAca) *pbx.ApplAca {
 		SchEndDate:     util.GoTimeToGrpcTime(data.SchEndDate),
 		StudyCampus:    data.StudyCampus,
 		StudyCenter:    data.StudyCenter,
-		ProgChoice_1:   data.ProgChoice1,
-		ProgChoice_2:   data.ProgChoice2,
-		ProgChoice_3:   data.ProgChoice3,
-		ProgChoice_4:   data.ProgChoice4,
-		ProgChoice_5:   data.ProgChoice5,
+		ProgChoice:     string(data.ProgChoice.RawMessage),
 		FeePaying:      data.FeePaying,
 		GradResTopic:   data.GradResTopic,
-		Referee_1:      string(data.Referee1.RawMessage),
-		Referee_2:      string(data.Referee2.RawMessage),
-		Referee_3:      string(data.Referee3.RawMessage),
-		Referee_4:      string(data.Referee4.RawMessage),
+		Referee:        string(data.Referee.RawMessage),
 		IsComplete:     data.IsComplete,
 		InstitutionId:  data.InstitutionID,
 
