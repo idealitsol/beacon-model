2b6776508271d3ceb20e1382d142c839c764afaf
Emmanuel Kofi Bessah <ekbessah@gmail.com>
2020-03-22 22:41:44 +0000

    Refactor external_exams with validation introduced

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/idealitsol/beacon-model
 go 1.12
 
 require (
+	github.com/go-playground/validator/v10 v10.2.0
 	github.com/idealitsol/beacon-proto v0.2.1
 	github.com/idealitsol/beacon-util v0.0.23
 	github.com/jinzhu/gorm v1.9.11
--- oma/external_exam.go
+++ oma/external_exam.go
@@ -2,20 +2,26 @@ package oma
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/idealitsol/beacon-proto/pbx"
 	util "github.com/idealitsol/beacon-util"
 	"github.com/jinzhu/gorm"
 	"github.com/jinzhu/gorm/dialects/postgres"
 )
 
+var validate *validator.Validate
+
 // ExternalExam database model
 type ExternalExam struct {
 	ID            string         `json:"id" gorm:"type:UUID;primary_key;size:36"`
-	ApplicantID   string         `json:"applicantId" gorm:"type:UUID;"`
-	WaecExam      postgres.Jsonb `json:"waecExam" gorm:"type:jsonb;default:'{}'"`
-	NonWaecExam   postgres.Jsonb `json:"nonWaecExam" gorm:"type:jsonb;default:'{}'"`
+	ApplicantID   string         `json:"applicantId" gorm:"type:UUID;" validate:"required"`
+	WaecExam      postgres.Jsonb `json:"waecExams" gorm:"type:jsonb;default:'{}'"`
+	NonWaecExam   postgres.Jsonb `json:"nonWaecExams" gorm:"type:jsonb;default:'{}'"`
 	Verified      bool           `json:"verified" gorm:"default:false"`
 	VerifiedBy    string         `json:"verifiedBy" gorm:"type:varchar(100);not null;default:''''''"`
 	CreatedAt     *time.Time     `json:"createdAt"`
@@ -25,27 +31,30 @@ type ExternalExam struct {
 	BXXUpdatedFields []string `json:"-" gorm:"-"`
 }
 
+// WaecExam for validation
 type WaecExam struct {
-	Level          string           `json:"level"`
-	IndexNo        string           `json:"indexNo"`
-	MonthYear      int32            `json:"monthYear"`
-	Sitting        int32            `json:"sitting"`
+	Level          string           `json:"level" validate:"required"`
+	IndexNo        string           `json:"indexNo" validate:"required"`
+	MonthYear      int32            `json:"monthYear" validate:"required"`
+	Sitting        int32            `json:"sitting" validate:"required"`
 	AwaitingResult bool             `json:"awaitingResult"`
-	Results        []WaecExamResult `json:"results"`
+	Results        []WaecExamResult `json:"results" validate:"required,dive,required"`
 }
 
+// WaecExamResult for validation
 type WaecExamResult struct {
-	SubjectCode int32  `json:"subjectCode"`
-	Grade       string `json:"grade"`
+	SubjectCode int32  `json:"subjectCode" validate:"required"`
+	Grade       string `json:"grade" validate:"required"`
 }
 
+// NonWaecExam for validation
 type NonWaecExam struct {
-	QualificationType      string     `json:"qualType"`
+	QualificationType      string     `json:"qualType" validate:"required"`
 	QualificationTypeOther string     `json:"qualTypeOther"`
-	Qualification          string     `json:"qualification"`
+	Qualification          string     `json:"qualification" validate:"required"`
 	IndexNo                string     `json:"indexNo"`
 	GPA                    string     `json:"gpa"`
-	Institution            string     `json:"institution"`
+	Institution            string     `json:"institution" validate:"required"`
 	ClassHonour            string     `json:"classHonour"`
 	AttendFrom             *time.Time `json:"attendFrom"`
 	AttendTo               *time.Time `json:"attendTo"`
@@ -108,6 +117,53 @@ func ExternalExamS2PTransformer(data ExternalExam) *pbx.ExternalExam {
 }
 
 func (o *ExternalExam) validate() (bool, error) {
+	validate = validator.New()
+	// register function to get tag name from json tags.
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+
+	if err := validate.Struct(o); err != nil {
+		verrs := err.(validator.ValidationErrors)
+		fmt.Println(verrs)
+		return false, fmt.Errorf("%s is %s", verrs[0].Field(), verrs[0].Tag())
+	}
+
+	// Validate WaecExam JSON by unmarshalling into struct to see if it's valid
+	var wes []WaecExam
+	if err := json.Unmarshal([]byte(string(o.WaecExam.RawMessage)), &wes); err != nil {
+		fmt.Printf("Json Unmarshal Error %v", err)
+		return false, fmt.Errorf("Invalid Waec Exam JSON property")
+	}
+	for _, we := range wes {
+		if err := validate.Struct(we); err != nil {
+			verrs := err.(validator.ValidationErrors)
+			fmt.Println(verrs)
+			return false, fmt.Errorf("%s is %s", verrs[0].Namespace(), verrs[0].Tag())
+		}
+	}
+
+	var nwes []NonWaecExam
+	if err := json.Unmarshal([]byte(string(o.NonWaecExam.RawMessage)), &nwes); err != nil {
+		fmt.Printf("Json Unmarshal Error %v", err)
+		return false, fmt.Errorf("Invalid Non Waec Exam JSON property")
+	}
+
+	for _, nwe := range nwes {
+		if err := validate.Struct(nwe); err != nil {
+			verrs := err.(validator.ValidationErrors)
+			fmt.Println(verrs)
+			return false, fmt.Errorf("%s is %s", verrs[0].Namespace(), verrs[0].Tag())
+		}
+	}
+
+	if len(wes) == 0 && len(nwes) == 0 {
+		return false, fmt.Errorf("Enter your WAEC or Non-WAEC Exams")
+	}
 
 	return true, nil
 }
